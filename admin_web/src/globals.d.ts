9c9f9225675b0138e7eedd4f5f5f46309d10b5f9
somulo1 <samuelokothomulo37@gmail.com>
2026-07-22 11:06:08 +0300

    add items

--- admin_web/src/globals.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "justgage";
-declare module "listree";
-declare module "tobii";
-declare module "vue3-clipboard";
-declare module "sweetalert2/dist/sweetalert2.js";
-declare module "mobius1-selectr";
-declare module "huebee";
